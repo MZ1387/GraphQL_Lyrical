12365517d1ae6636762fc3937516b401473ac6a7
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-11-27 14:38:12 -0500

    Update MONGO URI

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://me:password123@ds117834.mlab.com:17834/lyrical-db-dev';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
