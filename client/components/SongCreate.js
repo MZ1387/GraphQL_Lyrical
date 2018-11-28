f47d3897a6d503ab7f574b792ee287f58194d1e6
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-11-28 12:59:11 -0500

    Abstract fetchSongs query

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -18,7 +19,8 @@ class SongCreate extends Component {
     const { mutate } = this.props;
 
     mutate({
-      variables: { title }
+      variables: { title },
+      refetchQueries: [{ query }]
     }).then(() => hashHistory.push("/"))
   }
 
