5e9bbc1ee614a6fed6be0ef35c5781f58c2b56b8
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-11-29 17:45:20 -0500

    Update likes mutation

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -14,16 +14,19 @@ class LyricList extends Component {
   renderLyrics() {
     const { lyrics } = this.props;
 
-    return lyrics.map(({ id, content }) => {
+    return lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className="collection-item">
           {content}
-          <i
-            className="material-icons"
-            onClick={() => this.onLike(id)}
-          >
-            thumb_up
-          </i>
+          <div className="vote-box">
+            <i
+              className="material-icons"
+              onClick={() => this.onLike(id)}
+            >
+              thumb_up
+            </i>
+            {likes}
+          </div>
         </li>
       );
     });
